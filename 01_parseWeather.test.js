47bf0c9e4f6336a5d2b554d419a46bfbb09fc344
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-01-15 18:42:33 +0000

    add jest and first failing test

---
+++ 01_parseWeather.test.js
@@ -0,0 +1,20 @@
+/* 
+  All the exercises have some requirements: read them and translate them to test cases for each one. 
+  Often, some example input and expected output is provided: remove those from the exercise and use
+  them in your tests.
+
+  This file is for exercise1 only. Test file for the others exercises have to be created.
+*/
+
+const parseWeather = require('./01_parseWeather');
+
+describe('parseWeather', () => {
+  test('write a test!', () => {
+    const testInput = {};
+    const expectedResult = {};
+
+    const result = parseWeather(testInput);
+
+    expect(result).toEqual(expectedResult);
+  })
+});
